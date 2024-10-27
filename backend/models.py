88eeaf46b2b8e651b5f427737dd9c0afbc03521b
Bernardo Rivera <clase16bernardo@hotmail.com>
2024-10-27 13:55:31 -0500

    Se añade la columna categoría al Producto

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,4 @@
-# backend/models.py
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -10,6 +8,8 @@ class Producto(db.Model):
     descripcion = db.Column(db.String(200), nullable=True)
     precio = db.Column(db.Float, nullable=False)
     imagen = db.Column(db.String(100), nullable=True)  # URL de la imagen
+    categoria = db.Column(db.String(50), nullable=False)  # Columna de categoría
+
 
 class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key=True)
