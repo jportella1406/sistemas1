4567bc38477ac066d5b129efdb9d1dd7b591fca5
Bernardo Rivera <clase16bernardo@hotmail.com>
2024-10-27 15:15:36 -0500

    Se añaden cambios del Main

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
-# backend/config.py
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mercadito.db')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'mercadito.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
